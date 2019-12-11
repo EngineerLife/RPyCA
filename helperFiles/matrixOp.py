a629e49ebe4e1bbba8b57daa76f5f9f2b4d4da82
Marissa4 <mbennett@wpi.edu>
2019-12-11 14:47:29 -0500

    Cleaned up some files

--- helperFiles/matrixOp.py
+++ helperFiles/matrixOp.py
@@ -36,6 +36,37 @@ def oneHot(clmn):
     return onehot_encoded
 
 
+# mapping values function
+# USE:
+#    X = mapper(T, np.matrix(X))
+#    S = mapper(T, S)
+#    print("Mapped Dense S: ", S)
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+# maps the stuff
+def mapper(T, S):
+    # map values to 0-1
+    newS, newSS = [], []#np.matrix()
+    leftMax = np.amax(S)
+    for row in range(T.shape[0]):
+        for i in range(T.shape[1]):
+    #            print(S[row,i])
+            newS.append(translate(S[row,i], 0, leftMax, 0, 1))
+        newSS.append(newS)
+        newS = []
+    return np.matrix(newSS)
+
+
+
 # Used for randData function
 # !!! Must be global var bc func is recursive
 loopError = 0
--- helperFiles/stats.py
+++ helperFiles/stats.py
@@ -53,3 +53,31 @@ def printStats(filename):
     print("Malicious packets during:", malTime)
     print("# packets during", timeFrame1, "=", m)
     print("# packets during", timeFrame2, "=", a)
+
+
+
+# TODO create function for this
+
+# Computes TP, FP, TN, FN
+    '''
+    TP, FP, TN, FN = 0, 0, 0, 0
+    for row in range(T.shape[0]):
+        for feat in range(T.shape[1]):
+            # not an attack
+            if S[row, feat] < alpha:
+                if row in malPkts:  # an attack, False Negative
+                    FN += 1
+                else:   # not an attack, True Negative
+                    TN += 1
+            else:   # an attack
+                if row in malPkts:  # an attack, True Positive
+                    TP += 1
+                else:   # not an attack, False Positive
+                    FP += 1
+    # Compute the False Positive and True Positive Rates
+    FPR = FP*1./(FP + TN)
+    TPR = TP*1./(TP + FN)
+
+    print("Lambda: ", l, "  Alpha: ", alpha)
+    print("\nFPR:", FPR, "    TPR:", TPR)
+#    print("# of attacks: ", len(malPkts))
